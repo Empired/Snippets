bff868c4316cabd7c444d775c691295355eb3e53
David Miller <david.miller@obs.com.au>
2014-07-21 15:55:03 +1000

    added key lookup to prevent duplicate scripts

--- SharePoint 2013/Script/Nintex Forms/Load.js
+++ SharePoint 2013/Script/Nintex Forms/Load.js
@@ -1,4 +1,7 @@
 (function ($) {
+
+    $._loadJS = $._loadJS || [];
+
     function getParameterByName(input, name) {
         name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
         var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
@@ -14,24 +17,29 @@
         if (path.indexOf('/') != 0) {
             return;
         }
+        if ($.inArray(path, $._loadJS)) {
+            return;
+        }
+
+        $._loadJS.push(path);
         var promise = $.ajax(path, {
             async: false,
             cache: false,
             dataType: 'script'
         });
-		promise.fail(function() {
-			if (console.log) {
-				console.log('failed to request: '+path);
-			}
-		});
-        promise.done(function(content, status, xhr) {
-			if (content) {
-				var head = document.getElementsByTagName("head")[0];
-				var js = document.createElement("script");
-				js.type = "text/javascript";
-				js.text = content;
-				head.appendChild(js);
-			}
+        promise.fail(function () {
+            if (console.log) {
+                console.log('failed to request: ' + path);
+            }
+        });
+        promise.done(function (content, status, xhr) {
+            if (content) {
+                var head = document.getElementsByTagName("head")[0];
+                var js = document.createElement("script");
+                js.type = "text/javascript";
+                js.text = content;
+                head.appendChild(js);
+            }
         });
     }
 })(NWF$);
