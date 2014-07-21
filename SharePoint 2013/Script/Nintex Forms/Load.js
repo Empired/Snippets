fee31532b623e2267c293486f5a17e175777e06c
David Miller <david.miller@obs.com.au>
2014-07-21 15:25:34 +1000

    added synchrounous non-caching JS load for nintex.forms

---
+++ SharePoint 2013/Script/Nintex Forms/Load.js
@@ -0,0 +1,37 @@
+(function ($) {
+    function getParameterByName(input, name) {
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+            results = regex.exec(input);
+        return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+    var src = $('script:last').attr('src');
+    if (src) {
+        var path = getParameterByName(src, 'path');
+        if (path == "") {
+            return;
+        }
+        if (path.indexOf('/') != 0) {
+            return;
+        }
+        var promise = $.ajax(path, {
+            async: false,
+            cache: false,
+            dataType: 'script'
+        });
+		promise.fail(function() {
+			if (console.log) {
+				console.log('failed to request: '+path);
+			}
+		});
+        promise.done(function(content, status, xhr) {
+			if (content) {
+				var head = document.getElementsByTagName("head")[0];
+				var js = document.createElement("script");
+				js.type = "text/javascript";
+				js.text = content;
+				head.appendChild(js);
+			}
+        });
+    }
+})(NWF$);
